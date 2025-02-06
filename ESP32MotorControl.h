4dba1a65bdb7e878df4bd6332d1f6d516895d7e4
thomas302 <jtvelez2002@gmail.com>
2025-02-06 11:07:15 -0800

    Using doubles for speed instead of ints, old motorcontrol lib

--- ESP32MotorControl.h
+++ ESP32MotorControl.h
@@ -1,7 +1,5 @@
 /* Header for ESP32MotorControl
  *
- * Copyright (C) 2021  Karol Pieniacy https://github.com/pieniacy/ESP32MotorControl
- *
  * Copyright (C) 2018  Joao Lopes https://github.com/JoaoLopesF/ESP32MotorControl
  *
  * This program is free software: you can redistribute it and/or modify
@@ -25,49 +23,56 @@
 
 #include "Arduino.h"
 
+//////// Defines
+
 #ifndef ESP32
-#error "this library is only for ESP32"
+	#error "this library is only for ESP32"
 #endif
 
-class ESP32MotorControl {
+#define PWM_FREQ 1000	// PWM Frequency		
+
+//////// Class
+
+class ESP32MotorControl
+{
 public:
-	// Fields:
+
+	// Fields
+
 	uint16_t mMotorSpeed[2] = {0, 0};
 	boolean mMotorForward[2] = {true, true};
 
 	// Methods:
 
-	// Attach one motor
-	void attachMotor(uint8_t gpioIn1, uint8_t gpioIn2, uint32_t frequencyHz = 1000);
-	// Attach two motors
-	void attachMotors(uint8_t gpioIn1, uint8_t gpioIn2, uint8_t gpioIn3, uint8_t gpioIn4,
-	                  uint32_t frequencyHz = 1000);
+	void attachMotor(uint8_t gpioIn1, uint8_t gpioIn2);
+	void attachMotors(uint8_t gpioIn1, uint8_t gpioIn2, uint8_t gpioIn3, uint8_t gpioIn4);
 
-	void setMotor(uint8_t motor, double speed);
-	// Set speed -> PWM duty in the range 0-100
-	void motorForward(uint8_t motor, double speed);
-	void motorReverse(uint8_t motor, double speed);
-	// Set full speed
+	void motorForward(uint8_t motor, float speed);
 	void motorFullForward(uint8_t motor);
-	void motorFullReverse(uint8_t motor);
-	// Stop specific motor
+
+    void motorFullReverse(uint8_t motor);
+    void motorReverse(uint8_t motor, float speed);
+
+    void setMotorSpeed(uint8_t motor, float speed);
 	void motorStop(uint8_t motor);
-	// Stop both motors
+
 	void motorsStop();
-	// Set speed values of [-100 : 100] to both motors
-	void motorsSet(double speed0, double speed1);
+
+	void handle();
 
 	uint8_t getMotorSpeed(uint8_t motor);
 	boolean isMotorForward(uint8_t motor);
 	boolean isMotorStopped(uint8_t motor);
 
 private:
-	// Fields:
-	boolean mMotorAttached_[2] = {false, false};
 
-	// Methods:
-	void setMotor_(uint8_t motor, double speed);
-	boolean isMotorValid_(uint8_t motor);
+	// Fields:
+
+	boolean mMotorAttached[2] = {false, false};
+
+	// Methods
+
+	boolean isMotorValid(uint8_t motor);
 };
 
-#endif  // ESP32MotorControl_H
+#endif // ESP32MotorControl_H
